85b271b80703fc762dc93a70710dfc994a4c11dd
Groovesmith <jonas@octa-itmedia.dk>
2022-07-27 08:57:27 +0200

    send

--- index.js
+++ index.js
@@ -276,3 +276,83 @@ function formatDate(d) {
 
   return day + ' / ' + month + ' / ' + year;
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    initialDate: '2022-04-12',
+
+    eventDidMount: function(info) {
+      var tooltip = new Tooltip(info.el, {
+        title: info.event.extendedProps.description,
+        placement: 'top',
+        trigger: 'hover',
+        container: 'body'
+      });
+    },
+
+    events: [
+      {
+        title: 'All Day Event',
+        description: 'description for All Day Event',
+        start: '2022-04-01'
+      },
+      {
+        title: 'Long Event',
+        description: 'description for Long Event',
+        start: '2022-04-07',
+        end: '2022-04-10'
+      },
+      {
+        groupId: '999',
+        title: 'Repeating Event',
+        description: 'description for Repeating Event',
+        start: '2022-04-09T16:00:00'
+      },
+      {
+        groupId: '999',
+        title: 'Repeating Event',
+        description: 'description for Repeating Event',
+        start: '2022-04-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        description: 'description for Conference',
+        start: '2022-04-11',
+        end: '2022-04-13'
+      },
+      {
+        title: 'Meeting',
+        description: 'description for Meeting',
+        start: '2022-04-12T10:30:00',
+        end: '2022-04-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        description: 'description for Lunch',
+        start: '2022-04-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        description: 'description for Meeting',
+        start: '2022-04-12T14:30:00'
+      },
+      {
+        title: 'Birthday Party',
+        description: 'description for Birthday Party',
+        start: '2022-04-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        description: 'description for Click for Google',
+        url: 'http://google.com/',
+        start: '2022-04-28'
+      }
+    ]
+  });
+
+  calendar.render();
+});
